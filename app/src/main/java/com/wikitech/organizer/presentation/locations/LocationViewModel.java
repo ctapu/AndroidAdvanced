b54269b1c4017a9d44f1b4abb375834231059fb2
Ciprian MT <cipriantapu14@gmail.com>
2021-02-09 02:59:40 +0200

    Add implementation for LocationsFragment and LocationViewModel

---
+++ app/src/main/java/com/wikitech/organizer/presentation/locations/LocationViewModel.java
@@ -0,0 +1,25 @@
+package com.wikitech.organizer.presentation.locations;
+
+import android.util.Log;
+
+import androidx.databinding.ObservableArrayList;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.wikitech.organizer.domain.FetchLocationItemsUseCase;
+import com.wikitech.organizer.domain.LocationItem;
+
+import java.util.List;
+
+public class LocationViewModel extends ViewModel {
+
+    public static final String LOG_TAG = "Locations_tag";
+    public ObservableArrayList<LocationItem> locations = new ObservableArrayList<>();
+
+    public LocationViewModel(FetchLocationItemsUseCase fetchItemsUseCase){
+        Log.d(LOG_TAG, "My button was hit from view model");
+
+        LiveData<List<LocationItem>> liveItems = fetchItemsUseCase.execute();
+        liveItems.observeForever(locationItems -> locations.addAll(locationItems));
+    }
+}
---
+++ app/src/main/java/com/wikitech/organizer/presentation/locations/LocationsFragment.java
@@ -0,0 +1,56 @@
+package com.wikitech.organizer.presentation.locations;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.wikitech.organizer.R;
+import com.wikitech.organizer.data.local.LocationInMemoryDataSource;
+import com.wikitech.organizer.data.remote.LocationRemoteDataSource;
+import com.wikitech.organizer.data.api.LocationApi;
+import com.wikitech.organizer.databinding.FragmentLocationsBinding;
+import com.wikitech.organizer.domain.FetchLocationItemsUseCase;
+import com.wikitech.organizer.domain.LocationItemsMediator;
+import com.wikitech.organizer.domain.LocationItemsRepository;
+
+public class LocationsFragment extends Fragment {
+
+    private static final String LOG_TAG = "Locations";
+    private LocationViewModel locationViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(LOG_TAG,"LocationsFragment created");
+
+        ViewModelProvider.Factory factory = new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                FetchLocationItemsUseCase useCase = createUseCase();
+                return (T) new LocationViewModel(useCase);
+            }
+        };
+
+        LocationViewModel viewModel = new ViewModelProvider(this, factory).get(LocationViewModel.class);
+
+        FragmentLocationsBinding binding =
+                DataBindingUtil.setContentView(getActivity(), R.layout.fragment_locations);
+        binding.setLocationsViewModel(viewModel);
+    }
+
+      private FetchLocationItemsUseCase createUseCase() {
+        LocationItemsRepository localRepository = new LocationInMemoryDataSource();
+        LocationItemsRepository remoteRepository = new LocationRemoteDataSource(LocationApi.createApi());
+
+        LocationItemsMediator mediator = new LocationItemsMediator(localRepository, remoteRepository);
+
+        return new FetchLocationItemsUseCase(mediator);
+    }
+}
