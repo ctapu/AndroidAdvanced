82e39c12c1c08aa37f9fc61a19333d5f0eaad2b8
Ciprian MT <cipriantapu14@gmail.com>
2021-02-10 15:46:43 +0200

    add PeopleApi

---
+++ app/src/main/java/com/wikitech/organizer/data/remote/api/PeopleApi.java
@@ -0,0 +1,32 @@
+package com.wikitech.organizer.data.remote.api;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.wikitech.organizer.data.model.PersonItemDto;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public interface PeopleApi {
+    String BASE_URL = "TODO";
+
+    @GET("items.json")
+    Call<List<PersonItemDto>> getItems();
+
+    static PeopleApi createApi() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(PeopleApi.class);
+    }
+}
