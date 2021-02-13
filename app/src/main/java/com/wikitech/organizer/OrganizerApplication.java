ae80bbeb63e399ad85019c64c49777b47ab6ee47
Ciprian MT <cipriantapu14@gmail.com>
2021-02-13 17:26:23 +0200

    integrate crashlitics, timber and leak canary

--- app/src/main/java/com/wikitech/organizer/OrganizerApplication.java
+++ app/src/main/java/com/wikitech/organizer/OrganizerApplication.java
@@ -4,8 +4,17 @@ import android.app.Application;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
+
+import com.facebook.stetho.Stetho;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.wikitech.organizer.presentation.notification.NotificationChannelFactory;
+
+import org.jetbrains.annotations.NotNull;
+
+import timber.log.Timber;
 
 public class OrganizerApplication extends Application {
     public static OrganizerApplication instance;
@@ -15,10 +24,32 @@ public class OrganizerApplication extends Application {
         super.onCreate();
         instance = this;
 
+        setTools();
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             notificationManager.createNotificationChannel(NotificationChannelFactory.createProcessingWorkNotificationChannel());
         }
+    }
+
+    private void setTools() {
+        Stetho.initializeWithDefaults(this);
+
+        Timber.plant(new Timber.Tree() {
+            @Override
+            protected void log(int priority, @Nullable String tag, @NotNull String message, @Nullable Throwable throwable) {
+                if (priority < Log.INFO) {
+                    return;
+                }
+
+                if (throwable != null) {
+                    FirebaseCrashlytics.getInstance().recordException(throwable);
+                }
+
+                String crashlyticsMessage = String.format("[%s] %s", tag, message);
+                FirebaseCrashlytics.getInstance().log(crashlyticsMessage);
+            }
+        });
     }
 }
 
